5b2d87453d3f8cf4a3df90f0bc47277c66aac6cd
matheus23 <matheus23@users.noreply.github.com>
2025-11-17 09:31:09 +0000

    deploy: 5d0bf5376f3318827b4e9942bdfa482382744fd8

---
+++ pr/4/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["dasl",[["impl&lt;'de, R, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"dasl/drisl/de/struct.StreamDeserializer.html\" title=\"struct dasl::drisl::de::StreamDeserializer\">StreamDeserializer</a>&lt;'de, R, T&gt;<div class=\"where\">where\n    R: Read&lt;'de&gt;,\n    T: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt;,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[605]}
---
+++ pr/4/docs/trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["dasl",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"dasl/drisl/enum.Value.html\" title=\"enum dasl::drisl::Value\">Value</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"dasl/cid/struct.Cid.html\" title=\"struct dasl::cid::Cid\">Cid</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[510]}
