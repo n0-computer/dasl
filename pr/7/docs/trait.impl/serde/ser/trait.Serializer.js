3ffd7000c02be04b0ed1d2e60817de04f03c123f
flub <flub@users.noreply.github.com>
2026-03-16 09:03:49 +0000

    deploy: bfa3f01a9ec8354fccc77e20622c4c62938f9509

---
+++ pr/7/docs/trait.impl/serde/ser/trait.Serializer.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["dasl",[["impl&lt;'a, W: Write&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serializer.html\" title=\"trait serde::ser::Serializer\">Serializer</a> for &amp;'a mut <a class=\"struct\" href=\"dasl/drisl/ser/struct.Serializer.html\" title=\"struct dasl::drisl::ser::Serializer\">Serializer</a>&lt;W&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[336]}
